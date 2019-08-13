da31ee6f3a5038dc7ca292c319f029c07aa773ee
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-12 22:22:45 -0700

    pointers

---
+++ asm-pointers.asm
@@ -0,0 +1,31 @@
+TITLE Random Sorter(Assignment5.asm)
+
+; Author: Pedro Torres Picon(torrespp@oregonstate.edu)
+; Course / Project ID : CS271 - 400 / Programming Assignment #5
+; Due Date : 11 / 21 / 2016
+; Description: A program that takes an integer from the user
+; between 10 and 200, produces that many random numbers, prints
+; them, sorts them in descending order, calculates the median,
+; and displays the sorted list
+
+INCLUDE Irvine32.inc
+
+
+.data
+myVal		DWORD	?
+
+
+.code
+
+main PROC
+	
+	
+	
+	
+	exit					;exit to operating system
+
+main ENDP
+
+
+
+END main
